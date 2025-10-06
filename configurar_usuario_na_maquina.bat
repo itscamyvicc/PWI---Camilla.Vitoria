ba90c8cc0c99e73976600472920b2d961852df97
Camilla Vitoria Silva <camillav09@gmail.com>
2025-10-06 12:40:46 -0300

    Comando BAT para configurar meu usuário no computador.

--- configurar_usuario_na_maquina.bat
+++ configurar_usuario_na_maquina.bat
@@ -1,11 +1,11 @@
 echo OFF
 
-echo --- Retirando Usuario GitHUB ----
+echo --- Retirando Usuario GitHub ----
 
 git config --global --unset-all user.mail
 git config --global --unset-all user.name
 
-echo --- Configurando Usuario GitHUB ----
+echo --- Configurando Usuario GitHub ----
 
 git config --global user.name "Camilla Vitoria"
 git config --global user.email camillav09@gmail.com
@@ -13,6 +13,7 @@ git config --global user.email camillav09@gmail.com
 echo --- Mostrando a configuração atual do Github no computador
 
 git config --list
+
 echo --- Fim ---
 
 pause
